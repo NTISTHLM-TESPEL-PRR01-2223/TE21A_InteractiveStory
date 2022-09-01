7b8ab3443fb3c5ace2b69fa815e53b1a278e5b6c
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-01 18:59:21 +0200

    Initial commit

---
+++ TE21A_InteractiveStory/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Läs in namnet
+Console.WriteLine("Hej, vad heter du?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hej {name} ");
+
+if (name == "Micke")
+{
+  Console.WriteLine("Vad fin du är idag!");
+
+  Console.WriteLine("Vill1 du byta grejer?");
+  string byta = Console.ReadLine();
+  
+  if (byta == "ja")
+  {
+
+  }
+}
+
+// else if (name == "Martin")
+// {
+//   Console.WriteLine("Uuuuuuursch!");
+// }
+// else
+// {
+//   Console.WriteLine("Vad ful du är idag!");
+// }
+
+Console.ReadLine();
